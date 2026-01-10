6931d51ac66ee04e3fbf3ba208d2f41573af668e
python-guru <agris.simanis@gmail.com>
2026-01-10 09:43:13 +0000

    reenable ssid

--- ssid.tf
+++ ssid.tf
@@ -2,7 +2,7 @@ resource "meraki_wireless_ssid" "lab" {
   network_id = meraki_network.lab.id
 
   number          = "2"
-  name            = var.ssid_name
+  name            = "testtesttest"
   enabled         = false
   encryption_mode = "wpa"
   auth_mode       = "psk"
